fd504df095e2b642e3b2942d6792729bd27d67a3
kunge22 <hellchant22@me.com>
2017-09-15 20:17:04 +1000

    jj

---
+++ exercise/filter/assets/js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+
+$('.nav_ad').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.ad').removeClass('hide');
+  $('.item.ad').addClass('show');
+});
+
+$('.nav_plan').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.plan').removeClass('hide');
+  $('.item.plan').addClass('show');
+});
+
+$('.nav_pipe').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.pipe').removeClass('hide');
+  $('.item.pipe').addClass('show');
+});
+
+$('.nav_brick').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.brick').removeClass('hide');
+  $('.item.brick').addClass('show');
+});
+
+$('.nav_window').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.window').removeClass('hide');
+  $('.item.window').addClass('show');
+});
+
+
+$('.nav_all').click(function(event) {
+  $('.item').removeClass('hide');
+  $('.item').addClass('show');
+});
+
+$('.nav_thumbnail').click(function(event){
+  $('.item').removeClass('full');
+  $('.item').addClass('thumbnail');
+  $('.item').removeClass('col1of4');
+  $('.item').addClass('thumbnail');
+
+});
+
+$('.nav_full').click(function(event){
+  $('.item').removeClass('thumbnail');
+  $('.item').addClass('full');
+  $('.item').removeClass('col1of4');
+  $('.item').addClass('full');
+});
+
+$('.nav_default').click(function(event){
+  $('.item').removeClass('thumbnail');
+  $('.item').addClass('default');
+  $('.item').removeClass('full');
+  $('.item').addClass('default');
+});
+
+
+
+});
