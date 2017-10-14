d572849ec3f4137e696af2c710ec9f49ced18cad
KUNG E <hellchant22@me.com>
2017-10-15 03:33:32 +1100

    11

--- abouttime/assets/js/main.js
+++ abouttime/assets/js/main.js
@@ -3,16 +3,139 @@ $(document).ready(function() {
 
 // Add jQuery here
 
+$('.zodiac').click(function(event) {
+  $('.zodiacpage').removeClass('hide');
+  $('.zodiacpage').addClass('show');
+});
+
+$('.gaskins').click(function(event) {
+  $('.gaskinspage').removeClass('hide');
+  $('.gaskinspage').addClass('show');
+});
+
+$('.cooke').click(function(event) {
+  $('.cookepage').removeClass('hide');
+  $('.cookepage').addClass('show');
+});
+
+$('.miyazaki').click(function(event) {
+  $('.miyazakipage').removeClass('hide');
+  $('.miyazakipage').addClass('show');
+});
+
+$('.filho').click(function(event) {
+  $('.filhopage').removeClass('hide');
+  $('.filhopage').addClass('show');
+});
+
+$('.jack').click(function(event) {
+  $('.jackpage').removeClass('hide');
+  $('.jackpage').addClass('show');
+});
+
+$('.haarmann').click(function(event) {
+  $('.haarmannpage').removeClass('hide');
+  $('.haarmannpage').addClass('show');
+});
 
 
+$('.chikatilo').click(function(event) {
+  $('.chikatilopage').removeClass('hide');
+  $('.chikatilopage').addClass('show');
+});
 
 
+$('.dahmer').click(function(event) {
+  $('.dahmerpage').removeClass('hide');
+  $('.dahmerpage').addClass('show');
+});
+
+$('.yang').click(function(event) {
+  $('.yangpage').removeClass('hide');
+  $('.yangpage').addClass('show');
+});
+
+$('.wuornos').click(function(event) {
+  $('.wuornospage').removeClass('hide');
+  $('.wuornospage').addClass('show');
+});
+
+$('.lucas').click(function(event) {
+  $('.lucaspage').removeClass('hide');
+  $('.lucaspage').addClass('show');
+});
+
+ // cancel button start here
+
+$('.button').click(function(event) {
+  $('.zodiacpage').removeClass('show');
+  $('.zodiacpage').addClass('hide');
+});
+
+$('.button').click(function(event) {
+  $('.gaskinspage').removeClass('show');
+  $('.gaskinspage').addClass('hide');
+});
 
 
+$('.button').click(function(event) {
+  $('.cookepage').removeClass('show');
+  $('.cookepage').addClass('hide');
+});
 
 
+$('.button').click(function(event) {
+  $('.zodiacpage').removeClass('show');
+  $('.zodiacpage').addClass('hide');
+});
 
 
+$('.button').click(function(event) {
+  $('.miyazakipage').removeClass('show');
+  $('.miyazakipage').addClass('hide');
+});
+
+
+$('.button').click(function(event) {
+  $('.filhopage').removeClass('show');
+  $('.filhopage').addClass('hide');
+});
+
+
+$('.button').click(function(event) {
+  $('.jackpage').removeClass('show');
+  $('.jackpage').addClass('hide');
+});
+
+
+$('.button').click(function(event) {
+  $('.zodiacpage').removeClass('show');
+  $('.zodiacpage').addClass('hide');
+});
+
+
+$('.button').click(function(event) {
+  $('.haarmannpage').removeClass('show');
+  $('.haarmannpage').addClass('hide');
+});
+
+
+$('.button').click(function(event) {
+  $('.chikatilopage').removeClass('show');
+  $('.chikatilopage').addClass('hide');
+});
+
+
+$('.button').click(function(event) {
+  $('.dahmerpage').removeClass('show');
+  $('.dahmerpage').addClass('hide');
+});
+
+
+$('.button').click(function(event) {
+  $('.lucaspage').removeClass('show');
+  $('.lucaspage').addClass('hide');
+});
 
 
 
